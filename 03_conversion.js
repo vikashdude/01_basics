f31dfd1c2e58d619bec55f6326e5556ae707395e
vikashdude <matchlessvikash@gmail.com>
2024-11-12 21:15:19 +0530

    conversion

---
+++ 03_conversion.js
@@ -0,0 +1,22 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+
+// "33" =>33
+// "33abdg" =>NaN
+
+//"true" =>1; false=>0
+
+let isLoggedIn =true
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=>true;0=>flase
+//""=>false
+//"hitesh"=>true
--- test.js
+++
@@ -1,1 +0,0 @@
-console.log("Vikash Singh")
