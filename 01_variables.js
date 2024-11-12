01b63a168a05a76fe11326b53a6e3cbec3b387fb
vikashdude <matchlessvikash@gmail.com>
2024-11-12 13:16:22 +0530

    datatypes

---
+++ 01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+
+// accountId = 2  // not   aloowed
+
+accountEmail = "hi@hc.com"
+var accountPassword = "21212121"
+accountCity = "Bengalueru"
+
+/* prefer not to use var because of issue  in block scope and functional*/
+
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+
+
---
+++ 02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict";  //treat all  JS code as newer version
+
+// alert(3+3) we are using node js,not browser
+console.log(3+ 3) //code readability should be high
+
+
+
+console.log("Vikash singh")
+let name = "hitesh"  /*string type*/
+let age = 18    /*number type*/
+let isLoggedIn = flase /*Boolean type*/
+
+
+/* number => 2 to power 53 */
+/*bigint  use big number ke liye hota hai  like trading ,stock market me,*ya bahot badi website me hoti h*/
+/*string =>""*/
+// boolean => true/flase
+// null => standlone value h
+// undefined => 
+    // Symbol=> unique 
+// object =>
+
+console.log(typeof"hitesh");
+
+
+
+
+
+
+
